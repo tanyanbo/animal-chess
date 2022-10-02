87bf609f2b518f7a0daaa21377c93589759f636c
Tan Yanbo <tanyanbo95@hotmail.com>
2022-10-02 17:16:18 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ChessBoard from "./components/ChessBoard.vue";
+</script>
+
+<template>
+  <div class="outer-container">
+    <ChessBoard />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.outer-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/ChessBoard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const url = "../assets/mao.svg";
+const cat = `url(${url})`;
+</script>
+
+<template>
+  <div class="container">
+    <div
+      class="box"
+      v-for="(n, index) in 63"
+      :key="index"
+      :class="{ cat: index === 20 }"
+    ></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  display: grid;
+  width: 350px;
+  height: 450px;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(9, 1fr);
+
+  .box {
+    height: 50px;
+    width: 50px;
+    border: 1px solid black;
+
+    &.cat {
+      background-image: v-bind(cat);
+    }
+  }
+}
+</style>
