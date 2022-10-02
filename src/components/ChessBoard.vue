1d703dc5a7db95870ad8c6f55e2b63c9c1a6ab76
Tan Yanbo <tanyanbo95@hotmail.com>
2022-10-02 17:41:53 +0800

    Add traps and rivers

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
-const url = "../assets/mao.svg";
-const cat = `url(${url})`;
+const TRAPS = [2, 4, 10, 52, 58, 60];
+const RIVER = [22, 23, 25, 26, 29, 30, 32, 33, 36, 37, 39, 40];
 </script>
 
 <template>
   <div class="container">
-    <div
-      class="box"
-      v-for="(n, index) in 63"
-      :key="index"
-      :class="{ cat: index === 20 }"
-    ></div>
+    <div class="box" v-for="(n, index) in 63" :key="index">
+      <img
+        src="../assets/shu.svg"
+        alt="animal"
+        v-show="index === 22"
+        class="animal"
+      />
+      <img
+        src="../assets/grass.png"
+        alt="trap"
+        v-if="TRAPS.includes(index)"
+        class="trap"
+      />
+      <img
+        src="../assets/river.svg"
+        alt="river"
+        v-if="RIVER.includes(index)"
+        class="river"
+      />
+    </div>
   </div>
 </template>
 
@@ -26,9 +40,31 @@ const cat = `url(${url})`;
     height: 50px;
     width: 50px;
     border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
 
-    &.cat {
-      background-image: v-bind(cat);
+    img {
+      height: 90%;
+      width: 90%;
+      position: absolute;
+      top: 5%;
+      left: 5%;
+
+      &.trap,
+      &.river {
+        z-index: 0;
+      }
+
+      &.river {
+        height: 100%;
+        width: 100%;
+      }
+
+      &.animal {
+        z-index: 10;
+      }
     }
   }
 }
