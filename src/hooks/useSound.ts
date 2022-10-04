341ed30a13a103ca58509c1ce9157e909a7ee051
Tan Yanbo <tanyanbo95@hotmail.com>
2022-10-04 22:57:21 +0800

    Change sound file url in production

--- src/hooks/useSound.ts
+++ src/hooks/useSound.ts
@@ -1,17 +1,29 @@
 import { setTimeoutPromise } from "../helpers/setTimeoutPromise"
 
 export function useSound(volume: number, shouldPlaySound: boolean) {
-  const tigerSound = new Audio("/src/assets/sounds/tiger.mp3")
+  const tigerSound = new Audio(
+    new URL("../assets/sounds/tiger.mp3", import.meta.url).href
+  )
   tigerSound.volume = volume
-  const wolfSound = new Audio("/src/assets/sounds/wolf.mp3")
+  const wolfSound = new Audio(
+    new URL("../assets/sounds/wolf.mp3", import.meta.url).href
+  )
   wolfSound.volume = volume
-  const dogSound = new Audio("/src/assets/sounds/dog.mp3")
+  const dogSound = new Audio(
+    new URL("../assets/sounds/dog.mp3", import.meta.url).href
+  )
   dogSound.volume = volume
-  const catSound = new Audio("/src/assets/sounds/cat.mp3")
+  const catSound = new Audio(
+    new URL("../assets/sounds/cat.mp3", import.meta.url).href
+  )
   catSound.volume = volume
-  const mouseSound = new Audio("/src/assets/sounds/mouse.mp3")
+  const mouseSound = new Audio(
+    new URL("../assets/sounds/mouse.mp3", import.meta.url).href
+  )
   mouseSound.volume = volume
-  const elephantSound = new Audio("/src/assets/sounds/elephant.mp3")
+  const elephantSound = new Audio(
+    new URL("../assets/sounds/elephant.mp3", import.meta.url).href
+  )
   elephantSound.volume = volume
 
   const SOUND_DICT: Record<Animal, HTMLAudioElement> = {
