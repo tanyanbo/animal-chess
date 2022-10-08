51a485d070c2151666eb4adaab7c395fc08e3fe9
Tan Yanbo <tanyanbo95@hotmail.com>
2022-10-08 16:10:43 +0800

    remove semicolon

--- src/hooks/useSound.ts
+++ src/hooks/useSound.ts
@@ -1,30 +1,30 @@
-import { setTimeoutPromise } from "../helpers/setTimeoutPromise";
+import { setTimeoutPromise } from "../helpers/setTimeoutPromise"
 
 export function useSound(volume: number, shouldPlaySound: boolean) {
   const tigerSound = new Audio(
     new URL("../assets/sounds/tiger.mp3", import.meta.url).href
-  );
-  tigerSound.volume = volume;
+  )
+  tigerSound.volume = volume
   const wolfSound = new Audio(
     new URL("../assets/sounds/wolf.mp3", import.meta.url).href
-  );
-  wolfSound.volume = volume;
+  )
+  wolfSound.volume = volume
   const dogSound = new Audio(
     new URL("../assets/sounds/dog.mp3", import.meta.url).href
-  );
-  dogSound.volume = volume;
+  )
+  dogSound.volume = volume
   const catSound = new Audio(
     new URL("../assets/sounds/cat.mp3", import.meta.url).href
-  );
-  catSound.volume = volume;
+  )
+  catSound.volume = volume
   const mouseSound = new Audio(
     new URL("../assets/sounds/mouse.mp3", import.meta.url).href
-  );
-  mouseSound.volume = volume;
+  )
+  mouseSound.volume = volume
   const elephantSound = new Audio(
     new URL("../assets/sounds/elephant.mp3", import.meta.url).href
-  );
-  elephantSound.volume = volume;
+  )
+  elephantSound.volume = volume
 
   const SOUND_DICT: Record<number, HTMLAudioElement> = {
     64: tigerSound,
@@ -35,17 +35,17 @@ export function useSound(volume: number, shouldPlaySound: boolean) {
     4: dogSound,
     2: catSound,
     1: mouseSound,
-  };
+  }
 
   async function playSound(animal: number) {
-    SOUND_DICT[animal].play();
-    const duration = Math.ceil(SOUND_DICT[animal].duration * 1000);
+    SOUND_DICT[animal].play()
+    const duration = Math.ceil(SOUND_DICT[animal].duration * 1000)
     if (shouldPlaySound) {
-      await setTimeoutPromise(duration);
+      await setTimeoutPromise(duration)
     }
   }
 
   return {
     playSound,
-  };
+  }
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,3 @@
-type Animal = 1 | 2 | 4 | 8 | 16 | 32 | 64 | 128;
+type Animal = 1 | 2 | 4 | 8 | 16 | 32 | 64 | 128
 
-type Board = Uint16Array;
+type Board = Uint16Array
